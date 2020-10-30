fa1782c74e7118a03da16690295bc9b3b0d377fe
Logan Wright <logan.william.wright@gmail.com>
2020-10-31 00:52:00 +0100

    MISH.MASH

---
+++ mishmash/asdf.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  mishmash
+//
+//  Created by Logan Wright on 10/31/20.
+//
+
+import Foundation
+
+//print("Hello, World!")
+
+@main
+struct App {
+    static func main() throws {
+        print("hi")
+    }
+}
