bdc27d17249ed29b1fc0820946618d3e80078541
Logan Wright <logan.william.wright@gmail.com>
2020-11-04 07:10:15 +0100

    asdfljk

--- Tests/mishmashTests/CompositeTests.swift
+++ Tests/mishmashTests/CompositeTests.swift
@@ -8,55 +8,68 @@ class CompositeKeyTests: SieqlTersts {
 //        }
 //    }
 
-    func testIntrospect() throws {
-        struct MultiPrim: Schema {
-            let one = PrimaryKey<String>()
+    struct MultiPrim: Schema {
+        let one = PrimaryKey<String>()
 
-            let color = Column<String>()
-            let number = Column<Int>()
+        let group = Group()
+        let color = Column<String>()
+        let number = \Group.nummer
 
-            struct Group: PrimaryKeyGroup {
-                let _color = \MultiPrim.color
-                let _number = \MultiPrim.number
-            }
+        struct Group: PrimaryKeyGroup, CompositeColumn {
+            let _color = \MultiPrim.color
+            let _number = \MultiPrim.number
+            var constraint: Constraint = .unique
+
+            var nummer = Column<Int>()
         }
+    }
 
-        let prim = try MultiPrim.on(db) { _ in }
+    func testIntrospect() throws {
+        try db.prepare { MultiPrim.self }
+
+        let prim = try MultiPrim.on(db) { new in
+            // throws helpful error, test not broken :)
+//            new.color = "asdf"
+//            new.number = 9
+        }
         let all = _unsafe_force_hydrate_columns_on(prim)
         print(all)
         print("")
     }
-
     func testMultiPrimaryKey() throws {
-        struct Gamer: Schema {
-            @ConstraintGroup
-            var gamertag = PrimaryKey<String>()
-            @ConstraintGroup
-            var userId = PrimaryKey<String>()
 
-            //
-            var rating = Column<Int>()
-        }
-
-        try db.prepare {
-            Player.self
-            SoccerPlayer.self
-            Gamer.self
-        }
+        let all = _unsafe_force_hydrate_columns_on(MultiPrim())
+        print(all)
+        print()
+//        struct Gamer: Schema {
+//            @ConstraintGroup
+//            var gamertag = PrimaryKey<String>()
+//            @ConstraintGroup
+//            var userId = PrimaryKey<String>()
+//
+//            //
+//            var rating = Column<Int>()
+//        }
+//
+//        try db.prepare {
+//            Player.self
+//            SoccerPlayer.self
+//            Gamer.self
+//        }
     }
 
     func testGroupedUnique() {
-
-        ///// can not have same team && number
-        ///// but can have same key in other columns
-        struct HockeyPlayer: Schema {
-            class Group: UniqueKeyGroup {
-                let team = Column<String>()
-                let number = Column<Int>()
-                let nickname = Column<String>()
-            }
-            let group = Group()
-        }
+//
+//        ///// can not have same team && number
+//        ///// but can have same key in other columns
+//        struct HockeyPlayer: Schema {
+//            class Group: UniqueKeyGroup {
+//                let team = Column<String>()
+//                let number = Column<Int>()
+//                let nickname = Column<String>()
+//            }
+//            let group = Group()
+//        }
 
     }
 
